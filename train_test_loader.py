4475d3080c0a408fb4440b85170dfc885b4bd3d7
Dr Venkata Rajesh Kumar <60958340+DrVenkataRajeshKumar@users.noreply.github.com>
2020-04-18 01:38:00 +0530

    Update train_test_loader.py

--- train_test_loader.py
+++ train_test_loader.py
@@ -1,27 +1,111 @@
 import torch
-import torchvision
 
-def load(trainset,testset,seed=1,batch_size=128,num_workers=4,pin_memory=True):
+#Training & Testing Loops
+from tqdm import tqdm
+from tqdm import tqdm_notebook
+from PIL import Image
 
-	#Get the Train and Test Set
-	# trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
-	# testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
 
-	SEED = 1
+def train(model, device, train_loader, optimizer, criterion, epoch,scheduler = False):
+  model.train()
+  pbar = tqdm(train_loader)
+  correct = 0
+  processed = 0
+  for batch_idx, (data, target) in enumerate(pbar):
+    # get samples
+    data, target = data.to(device), target.to(device)
 
-	# CUDA?
-	cuda = torch.cuda.is_available()
+    # Init
+    optimizer.zero_grad()
+    # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
+    # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
 
-	# For reproducibility
-	torch.manual_seed(SEED)
+    # Predict
+    y_pred = model(data)
 
-	if cuda:
-			torch.cuda.manual_seed(SEED)
+    # Calculate loss
+    #loss = F.nll_loss(y_pred, target)
+    loss = criterion(y_pred, target)
+    
 
-	# dataloader arguments - something you'll fetch these from cmdprmt
-	dataloader_args = dict(shuffle=True, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory) if cuda else dict(shuffle=True, batch_size=64)
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    if(scheduler):
+      scheduler.step()
 
-	trainloader = torch.utils.data.DataLoader(trainset, **dataloader_args)
-	testloader = torch.utils.data.DataLoader(testset, **dataloader_args)
+    # Update pbar-tqdm
+    
+    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    correct += pred.eq(target.view_as(pred)).sum().item()
+    processed += len(data)
 
-	return  trainloader, testloader
+    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+  train_losses.append(loss)
+  train_acc.append(100*correct/processed)
+  return train_losses,train_acc
+
+
+def test(model, device, criterion, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in tqdm(test_loader):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+	
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    return test_losses,test_acc
+
+
+
+test_loss = []
+test_acc = []
+train_loss = []
+train_loss = []
+LR = []
+train_scheduler = False
+
+def train_model(model,device,trainloader,testloader,optimizer,criterion,EPOCHS,scheduler = False,batch_scheduler = False ,best_acc = 0,path = "/content/gdrive/My Drive/API/bestmodel.pt"):
+  for epoch in range(EPOCHS):
+      print("EPOCH:", epoch+1,'LR:',optimizer.param_groups[0]['lr'])
+      LR.append(optimizer.param_groups[0]['lr'])
+      train_scheduler = False
+
+      if(batch_scheduler):
+        train_scheduler = scheduler
+      train_loss, train_acc = train(model, device, trainloader, optimizer, criterion, epoch,train_scheduler)
+      
+      test_loss , test_acc = test(model, device, criterion, testloader)
+      if(scheduler and not batch_scheduler and not isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau)): 
+        scheduler.step()
+
+      elif(scheduler and not batch_scheduler and  isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau)):
+        scheduler.step(test_loss[-1])
+      
+      
+      if(test_acc[-1]>best_acc):
+        print("accuracy increased, Saving model....")
+        best_acc = test_acc[-1]
+        torch.save({
+              'epoch': epoch,
+              'model_state_dict': model.state_dict(),
+              'optimizer_state_dict': optimizer.state_dict(),
+              'loss': test_loss[-1],
+              }, path)
+
